0cc6ff4da8e8d82c81cd27e6a52d21cc54700bb4
Stewart ilondanga <stilondanga@gmail.com>
2018-11-02 13:51:48 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,24 @@ var navigate = (function() {
     $('.' + dataName).toggle();
   });
 })();
-/*
-// Questions Array
-const questions = [
-  { question: 'Enter Your First Name' },
-  { question: 'Enter Your Last Name' },
-  { question: 'Enter Your Email', pattern: /\S+@\S+\.\S+/ },
-  { question: 'Create A Password', type: 'password' }
-];
 
+// Questions Array
+const questions = [{
+    question: 'Enter Your First Name'
+  },
+  {
+    question: 'Enter Your Last Name'
+  },
+  {
+    question: 'Enter Your Email',
+    pattern: /\S+@\S+\.\S+/
+  },
+  {
+    question: 'Create A Password',
+    type: 'password'
+  }
+];
+/*
 // Transition Times
 const shakeTime = 100; // Shake Transition Time
 const switchTime = 200; // Transition Between Questions
